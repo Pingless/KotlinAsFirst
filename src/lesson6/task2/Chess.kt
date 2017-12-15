b05454974c945f81025218dee5eeca092e574197
Pingless <ashotiqqq@gmail.com>
2017-12-16 02:16:52 +0300

    6th lesson

--- src/lesson6/task2/Chess.kt
+++ src/lesson6/task2/Chess.kt
@@ -1,6 +1,8 @@
 @file:Suppress("UNUSED_PARAMETER")
 package lesson6.task2
 
+import java.lang.StrictMath.abs
+
 /**
  * Клетка шахматной доски. Шахматная доска квадратная и имеет 8 х 8 клеток.
  * Поэтому, обе координаты клетки (горизонталь row, вертикаль column) могут находиться в пределах от 1 до 8.
@@ -21,7 +23,11 @@ data class Square(val column: Int, val row: Int) {
      * В нотации, колонки обозначаются латинскими буквами от a до h, а ряды -- цифрами от 1 до 8.
      * Для клетки не в пределах доски вернуть пустую строку
      */
-    fun notation(): String = TODO()
+    fun notation(): String {
+        if (inside() == false) return ""
+        val col = listOf<String>("a", "b", "c", "d", "e", "f", "g", "h")
+        return col[column - 1] + "$row"
+    }
 }
 
 /**
@@ -31,7 +37,12 @@ data class Square(val column: Int, val row: Int) {
  * В нотации, колонки обозначаются латинскими буквами от a до h, а ряды -- цифрами от 1 до 8.
  * Если нотация некорректна, бросить IllegalArgumentException
  */
-fun square(notation: String): Square = TODO()
+fun square(notation: String): Square {
+    val col = listOf<Char>('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h')
+    val ro = listOf<Char>('1', '2', '3', '4', '5', '6', '7', '8')
+    if (notation.length != 2 || notation[0] !in col || notation[1] !in ro) throw IllegalArgumentException()
+    return Square(col.indexOf(notation[0]) + 1, ro.indexOf(notation[1]) + 1)
+}
 
 /**
  * Простая
@@ -56,7 +67,12 @@ fun square(notation: String): Square = TODO()
  * Пример: rookMoveNumber(Square(3, 1), Square(6, 3)) = 2
  * Ладья может пройти через клетку (3, 3) или через клетку (6, 1) к клетке (6, 3).
  */
-fun rookMoveNumber(start: Square, end: Square): Int = TODO()
+fun rookMoveNumber(start: Square, end: Square): Int = when{
+    !start.inside() || !end.inside() -> throw IllegalArgumentException()
+    start == end -> 0
+    start.column == end.column || start.row == end.row -> 1
+    else -> 2
+}
 
 /**
  * Средняя
@@ -72,7 +88,12 @@ fun rookMoveNumber(start: Square, end: Square): Int = TODO()
  *          rookTrajectory(Square(3, 5), Square(8, 5)) = listOf(Square(3, 5), Square(8, 5))
  * Если возможно несколько вариантов самой быстрой траектории, вернуть любой из них.
  */
-fun rookTrajectory(start: Square, end: Square): List<Square> = TODO()
+fun rookTrajectory(start: Square, end: Square): List<Square> {
+    if (!start.inside() || !end.inside()) throw IllegalArgumentException()
+    if (start == end) return listOf(start)
+    if (start.column == end.column || start.row == end.row) return listOf(start, end)
+    return listOf(start, Square(start.column, end.row), end)
+}
 
 /**
  * Простая
@@ -97,7 +118,16 @@ fun rookTrajectory(start: Square, end: Square): List<Square> = TODO()
  * Примеры: bishopMoveNumber(Square(3, 1), Square(6, 3)) = -1; bishopMoveNumber(Square(3, 1), Square(3, 7)) = 2.
  * Слон может пройти через клетку (6, 4) к клетке (3, 7).
  */
-fun bishopMoveNumber(start: Square, end: Square): Int = TODO()
+fun bishopMoveNumber(start: Square, end: Square): Int {
+    when {
+        !start.inside() || !end.inside() -> throw IllegalArgumentException()
+        start == end -> return 0
+        abs(start.column - end.column) == abs(start.row - end.row) -> return 1
+        start.column % 2 == end.column % 2 && start.row % 2 != end.row % 2 -> return -1
+        start.column % 2 != end.column % 2 && start.row % 2 == end.row % 2 -> return -1
+        else -> return 2
+    }
+}
 
 /**
  * Сложная
@@ -139,7 +169,13 @@ fun bishopTrajectory(start: Square, end: Square): List<Square> = TODO()
  * Пример: kingMoveNumber(Square(3, 1), Square(6, 3)) = 3.
  * Король может последовательно пройти через клетки (4, 2) и (5, 2) к клетке (6, 3).
  */
-fun kingMoveNumber(start: Square, end: Square): Int = TODO()
+fun kingMoveNumber(start: Square, end: Square): Int {
+    when {
+        (start.inside() && end.inside()) -> return Math.max(Math.abs(end.column - start.column), Math.abs(end.row - start.row))
+        else -> throw IllegalArgumentException("Incorrect cells")
+    }
+}
+
 
 /**
  * Сложная
